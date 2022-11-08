c88ecd64826e8f710e776a8a3e3b8a2fa8bded20
chenquan886 <918780592@qq.com>
2022-11-08 15:47:24 +0800

    dev分支第一次提交,更新Main.java文件

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello");
+        System.out.println("创建新dev分支,提交内容");
     }
 }
